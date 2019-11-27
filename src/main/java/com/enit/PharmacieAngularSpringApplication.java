7a4bf7fd2a4018c7aa7f8c4ef029d13cf11398a8
Rania SMAOUI <rania.smaoui@etudiant-enit.utm.tn>
2019-11-27 08:07:37 +0000

    my first commit

---
+++ src/main/java/com/enit/PharmacieAngularSpringApplication.java
@@ -0,0 +1,12 @@
+package com.enit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacieAngularSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacieAngularSpringApplication.class, args);
+	}
+}
